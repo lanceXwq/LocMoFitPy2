5e3a31990975418ba15bcc9f3373ad1add983331
Lance (Weiqing) Xu <lance.xwq@gmail.com>
2026-01-28 16:07:56 +0100

    add doc to run.m

--- run.m
+++ run.m
@@ -1,19 +1,29 @@
-pe = pyenv(Version="/Users/lancexwq/Ries Lab/LocMoFitPy2/.pixi/envs/default/bin/python", ExecutionMode= "OutOfProcess");
+% Uncomment the two lines below for a fresh start
+% terminate(pyenv)
+% clear
 
+% Start Python
+pyenv(Version="/Users/lancexwq/Ries Lab/LocMoFitPy2/.pixi/envs/default/bin/python", ExecutionMode= "OutOfProcess");
+
+% Load data from CSV
 data = readmatrix("./notebooks/data/simulated_data_spcap.csv");
 locs = data(:, 1:3);
 loc_precisions = data(:,4:6);
 
 tic
+% Import the API
 bridge_dir = "/Users/lancexwq/Ries Lab/LocMoFitPy2";
 if count(py.sys.path, bridge_dir) == 0
     insert(py.sys.path, int32(0), bridge_dir);
 end
 api = py.importlib.import_module("matlab_api");
 
+% Set initial value and parameters to freeze/fix
 kwargs = py.dict;
-freeze = py.tuple({});
+% kwargs{"c"} = 1;
+freeze = py.tuple();
 
+% Run the code to fit
 res = api.run_locmofit( ...
     "SphericalCap", ...
     locs, ...
@@ -22,14 +32,14 @@ res = api.run_locmofit( ...
         "seed", int64(3), ...
         "model_init_kwargs", kwargs, ...
         "freeze", freeze, ...
-        "max_iter", int64(200), ...
+        "max_iter", int64(600), ...
         "tol", 1e-12 ...
     ) ...
 );
 
+% Collect the result
 final_loss = double(res{"final_loss"});
 positions  = double(res{"positions"});
-params_py  = res{"params"};
 toc
 
 
