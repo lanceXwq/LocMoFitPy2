9a542ad78ee87ee19573e71a711e312bc8a1dcd5
Lance (Weiqing) Xu <lance.xwq@gmail.com>
2026-01-30 00:18:22 +0100

    fix test and improve the matlab script

--- tests/test_spcap_fit.py
+++ tests/test_spcap_fit.py
@@ -7,7 +7,9 @@ def test_spcap_fit():
     locs_df = pd.read_csv("tests/example_spcap_data.csv")
     locs = locs_df.values[:, 0:3]
     stddev = locs_df.values[:, 3:]
-    expected_final_loss = 253.7626953125
-    res = run_locmofit("SphericalCap", locs, stddev, freeze=(), max_iter=200, tol=1e-6)
+    expected_final_loss = 96.35659
+    res = run_locmofit(
+        "SphericalCap", locs, stddev, freeze=(), max_iter=200, tol=1e-6, spacing=3.0
+    )
     actual_final_loss = res["losses"][-1]
     assert abs(actual_final_loss - expected_final_loss) / expected_final_loss < 1e-6
